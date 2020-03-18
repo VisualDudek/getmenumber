18b61976a17006a60c9696c376701365e55f4a15
Visual <dudek.v@gmail.com>
2020-03-18 22:36:59 +0100

    exceptions handling

--- getmedigit.py
+++ getmedigit.py
@@ -29,10 +29,21 @@ def number(url):
     img = img.point(lambda x: 0 if x < 200 else 255) 
     img_negatywne = img.getchannel('R')
 
-    return list(map(int, (
-        pytesseract.image_to_string(img_badania, lang='eng', config='--psm 6 --oem 3'), \
-        pytesseract.image_to_string(img_negatywne, lang='eng', config='--psm 6 --oem 3'),
-    )))
+    try:
+        res_badania = pytesseract.image_to_string(img_badania, lang='eng', config='--psm 6 --oem 3')
+        res_badania = int(res_badania)
+    except ValueError:
+        img_badania.save('log_img', 'JPEG')
+        raise Exception(f'OCR return non digit while processing BADANIA: {res_badania}, chekc log_img.jpeg')
+
+    try:
+        res_negatywne = pytesseract.image_to_string(img_negatywne, lang='eng', config='--psm 6 --oem 3')
+        res_negatywne = int(res_negatywne)
+    except ValueError:
+        img_negatywne.save('log_img', 'JPEG')
+        raise Exception(f'OCR return non digit while processing NEGATYWNE: {res_negatywne}, check log_img.jpeg')
+
+    return (res_badania, res_negatywne)
 
 if __name__ == "__main__":
 
@@ -42,7 +53,7 @@ if __name__ == "__main__":
         ('https://pbs.twimg.com/media/ETYlu89WsAYZrwr?format=jpg&name=4096x4096', '9515', '9269'),
         ('https://pbs.twimg.com/media/ETTee-AXYAMo_mc?format=jpg&name=4096x4096', '7899', '7694'),
         ('https://pbs.twimg.com/media/ETJLirdWAAENj2v?format=jpg&name=4096x4096', '5493', '5382'),
-        # ('https://pbs.twimg.com/media/ETEd72dWkAE8Cy1?format=jpg&name=4096x4096', '4414'),
+        ('https://pbs.twimg.com/media/ETEd72dWkAE8Cy1?format=jpg&name=4096x4096', '4414', '0000'),
         ('https://pbs.twimg.com/media/ES-ctkBWkAAUPku?format=jpg&name=4096x4096', '2889', '2831'),
         ('https://pbs.twimg.com/media/ES5xff1WsAEzLW4?format=jpg&name=4096x4096', '2234', '2187'),
         ('https://pbs.twimg.com/media/ES0QpA0XsAAaKg8?format=jpg&name=4096x4096', '2024', '1999'),
